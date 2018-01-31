bff5d76e4a6cc4b018566c4c869b6c5006811d41
Meredith Derecho <mnd@Merediths-MBP.wireless.yale.internal>
2018-01-30 21:17:26 -0500

    finished articles + added resulting text file

--- articles.py
+++ articles.py
@@ -1,65 +1,61 @@
+# Extract text of articles from 习近平重要讲话数据库
+# Write it into a file with a name taken from user input
+
 import sys
 import urllib.request
 from collections import OrderedDict
 from bs4 import BeautifulSoup
 
-# Extract text of articles from 习近平重要讲话数据库
-# Write it into a file with a name taken from user input
+# homepage of the website
+index = 'http://jhsjk.people.cn/'
 
-index = "http://jhsjk.people.cn/"
+# total number of articles
+n = 0
+
+# filename from user input
+filename = input('Name of new file: ')
+
+# ordered dictionary of categories of articles
+# keys are category names; values are article url lists
+# ex: 经济, ['article/29780020', ...]
+data = OrderedDict()
 
 
-# fix this!! so close <3
-def writeArticles(data, n):
-	pages = 'Total Articles: ' + str(n)
-	filename = input("Type name of new file: ")
-	f = open(filename, 'w')
-	f.write(pages)
-	f.write('# of total articles')
-	for category in data:
-		f.write(category.key)
-		for article in category.value:
-			url = index + str(article)
-			info = oneArticle(url)
-			f.write(info[0])
-			f.write(info[1])
-	f.close
-
-# given an article url and filename, 
-# write the article (title and contents) to the file
-def oneArticle(url):
-	page = urllib.request.urlopen(url).read()
+# populates n and data
+# by identifying the categories calling oneCategory
+def categories():
+	page = urllib.request.urlopen(index).read()
 	soup = BeautifulSoup(page, 'html.parser')
-	# find title and content of speech
-	title = soup.find('h1').get_text()
-	speech = soup.find('div', class_='d2txt_con').get_text()
-	# write to file
-	return[title, speech]
+	box = soup.find('div', class_='w1014')
+	links = box('a')
+	global n
+	global data
+	for link in links:
+		url = index + str(link.get('href'))
+		name = str(link.find('img').get('title'))
+		data[name] = []
+		print('Currently finding ' + name + 'category')
+		oneCategory(url, data[name])
+		n += len(data[name])
 
-def onePage():
-	'hi'
 
-# target = input("Name of file to write to: ")
-
-# http://jhsjk.people.cn/result?type=101
-
-# given the url of the first page
-# prints out all the article urls for all in that category
+# given the url of the first page and a list of articles
+# adds article urls in that category to articles
 def oneCategory(url, articles):
-	print('')
-	print('----page----')
 	page = urllib.request.urlopen(url).read()
 	soup = BeautifulSoup(page, 'html.parser')
+
+	# find and add article links
 	box = soup.find('div', class_='fr')
 	links = box.find('ul')('li')
 	for link in links:
-		articles.append(link.find('a').get('href'))
-	print(url)
+		articles.append(str(link.find('a').get('href')))
+
+	# recursively call function on next page of articles
 	pagination = box.find('div', class_='pagination')
 	if pagination == None:
 		return
 	numbers = pagination.find('ul')('li')
-	print(numbers)
 	item = str(numbers[0])
 	count = 1
 	while 'active' not in item and count < len(numbers):
@@ -69,44 +65,50 @@ def oneCategory(url, articles):
 		return
 	else:
 		next = numbers[count]
-		print(next)
 		nextLink = next.find('a').get('href')
 		oneCategory(nextLink, articles)
 
-def categories():
-	page = urllib.request.urlopen(index).read()
+
+# writes the contents of n and data to a file
+# filename taken by user input
+def writeArticles():
+	global filename
+	f = open(filename, 'w')
+
+	# write number of articles
+	pages = 'Total Articles: ' + str(n)
+	f.write(pages)
+	f.write('')
+
+	# write articles
+	count = 1
+	for key, value in data.items():
+		print('Currently writing ' + key + 'category')
+		f.write(key)
+		for article in value:
+			url = index + article
+			info = oneArticle(url)
+			f.write(str(count) + ': ' + url)
+			f.write(info[0])
+			f.write(info[1])
+			count += 1
+	f.close
+
+
+# given an article url and filename, 
+# return a list of the article title + speech contents
+def oneArticle(url):
+	page = urllib.request.urlopen(url).read()
 	soup = BeautifulSoup(page, 'html.parser')
-	box = soup.find('div', class_='w1014')
-	links = box('a')
-	n = 0						# number of total articles
-	data = OrderedDict()
-	for link in links:
-		url = index + str(link.get('href'))
-		print(url)
-		name = (link.find('img').get('title'))
-		data[name] = []
-		oneCategory(url, data[name])
-		n += len(data[name])
-	writeArticles(data, n)
 
-# jingji_pages = int(input("Number of 经济 pages: "))
-# zhengzhi_pages = int(input("Number of 经济 pages: "))
-# jingji_pages = int(input("Number of 经济 pages: "))
-# jingji_pages = int(input("Number of 经济 pages: "))
-# jingji_pages = int(input("Number of 经济 pages: "))
-# jingji_pages = int(input("Number of 经济 pages: "))
-# jingji_pages = int(input("Number of 经济 pages: "))
+	# find title and content of speech
+	title = soup.find('h1').get_text()
+	speech = soup.find('div', class_='d2txt_con').get_text()
+	return[title, speech]
 
-# http://jhsjk.people.cn/article/27847383
 
-# aurl = 'http://jhsjk.people.cn/article/27847383'
-# afile = input("prompt: ")
-
+# call the functions
 categories()
-
-# writeArticle(afile, aurl)
-# oneCategory('http://jhsjk.people.cn/result/3?type=108')
-
-# go to the pages after the active one
+writeArticles()
 
 
